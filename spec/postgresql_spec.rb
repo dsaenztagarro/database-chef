a0c732d2551fe90cba23d98b8290d1ebd8f00cdc
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-28 22:45:21 +0100

    Added posgresql spec

---
+++ spec/postgresql_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'database_sl::postgresql' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04') do |node|
+      node.set['postgresql']['version'] = '9.4'
+    end.converge described_recipe
+  end
+
+  # before(:each) do
+  #   stub_command('ls /var/lib/postgresql/9.3/main/recovery.conf').and_return(true)
+  # end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+end
