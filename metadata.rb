d01ecc2da9bf5a6dfce58b4f6af3d0a7045e01aa
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-29 18:38:27 +0100

    Added metadata info

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'MIT'
 description      'Installs/Configures database_sl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe 'database_sl::postgresql', 'Installs PostgreSQL db server and client'
+
+%w(debian ubuntu).each { |os| supports os }
