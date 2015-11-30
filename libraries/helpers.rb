2f166400e78a39fae196ef486bb8d5e9cbbdd1d1
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-11-30 15:43:47 +0100

    Extracter connection_options helper

---
+++ libraries/helpers.rb
@@ -0,0 +1,10 @@
+# Connection options:
+#   -h, --host=HOSTNAME      database server host or socket directory (default: "/var/run/postgresql")
+#   -p, --port=PORT          database server port (default: "5432")
+#   -U, --username=USERNAME  database user name (default: "vagrant")
+#   -w, --no-password        never prompt for password
+#   -W, --password           force password prompt (should happen automatically)
+def connection_options
+  conn = default_connection.merge(connection)
+  "-h #{conn[:hostname]} -p #{conn[:port]} -U #{conn[:user]} -w"
+end
--- resources/user.rb
+++ resources/user.rb
@@ -8,17 +8,6 @@ def default_connection
   { hostname: 'localhost', port: 5432, user: 'postgres' }
 end
 
-# Connection options:
-#   -h, --host=HOSTNAME      database server host or socket directory (default: "/var/run/postgresql")
-#   -p, --port=PORT          database server port (default: "5432")
-#   -U, --username=USERNAME  database user name (default: "vagrant")
-#   -w, --no-password        never prompt for password
-#   -W, --password           force password prompt (should happen automatically)
-def connection_options
-  conn = default_connection.merge(connection)
-  "-h #{conn[:hostname]} -p #{conn[:port]} -U #{conn[:user]} -w"
-end
-
 action :create do
   # Chef::Log.info("postgresql_user[#{resource_name}]")
 
