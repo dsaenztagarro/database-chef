5d751ac3b2699c8823a6ef3de1bedee39ee5a502
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-01 13:32:43 +0100

    Removed deprecation simplecov multiformatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,12 @@ require 'simplecov'
 require 'simplecov-console'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  Coveralls::SimpleCov::Formatter,
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::Console
-]
-SimpleCov.minimum_coverage(100)
+formatters = [Coveralls::SimpleCov::Formatter,
+              SimpleCov::Formatter::HTMLFormatter,
+              SimpleCov::Formatter::Console]
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(formatters)
+# SimpleCov.minimum_coverage(100)
 SimpleCov.start do
   add_group 'Recipes', 'recipes'
 end
