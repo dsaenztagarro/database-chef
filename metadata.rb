83672241799b8ac0ff967a02cebde637dc0151ce
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-01 13:06:48 +0100

    Added source and issue urls to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,11 @@ maintainer_email 'david.saenz.tagarro@gmail.com'
 license          'MIT'
 description      'Installs/Configures database_sl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 recipe 'database_sl::postgresql', 'Installs PostgreSQL db server and client'
 
 %w(debian ubuntu).each { |os| supports os }
+
+source_url 'https://github.com/dsaenztagarro/database_sl' if respond_to?(:source_url)
+issues_url 'https://github.com/dsaenztagarro/database_sl/issues' if respond_to?(:issues_url)
