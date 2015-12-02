54fcc8a67649ecc9c853f474e4032db6cc308260
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-02 23:25:55 +0100

    Added chefspecs for system requirements

---
+++ spec/system_requirements_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'database_sl::system_requirements' do
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04') do |node|
+      node.set['database_sl']['postgresql']['version'] = '9.4'
+    end.converge described_recipe
+  end
+
+  it 'runs a execute when specifying system locales' do
+    expect(chef_run).to run_execute('system_locales')
+  end
+
+  it 'installs all packages when given `wget, ca-certificates`' do
+    expect(chef_run).to install_package(['wget', 'ca-certificates'])
+  end
+end
