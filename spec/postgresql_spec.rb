82e8b243bf0feb7d256df43c0d5e93e31548a66e
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-02 13:43:30 +0100

    Removed commented code

--- spec/postgresql_spec.rb
+++ spec/postgresql_spec.rb
@@ -7,10 +7,6 @@ describe 'database_sl::postgresql' do
     end.converge described_recipe
   end
 
-  # before(:each) do
-  #   stub_command('ls /var/lib/postgresql/9.3/main/recovery.conf').and_return(true)
-  # end
-
   it 'converges successfully' do
     expect { :chef_run }.to_not raise_error
   end
