a1bac80fbb06d02f2f8e7347e4eb3f8b6bd7be83
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-12-05 03:26:42 +0100

    Updated version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'david.saenz.tagarro@gmail.com'
 license          'MIT'
 description      'Installs/Configures database_sl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 recipe 'database_sl::postgresql', 'Installs PostgreSQL db server and client'
 
